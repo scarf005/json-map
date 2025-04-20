5e4542319e1a2d721490c7723442a6f8ae8beb50
scarf <greenscarf005@gmail.com>
2025-04-20 21:52:27 +0900

    docs: fix module docs formatting

--- mod.ts
+++ mod.ts
@@ -1,6 +1,4 @@
 /**
- * @module
- *
  * JSON `parse` and `stringify` that uses [`Map`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map) instead of `Object`. Can be used in a niche situation when you need to preserve the order of keys in JSON objects for some reason.
  *
  * Implemented as a thin wrapper around serde-json.
@@ -12,6 +10,7 @@
  *
  * parse(`{ ".": 1, "$": 2, "1": 3 }`)
  * ```
+ * @module
  */
 export {
   parse,
