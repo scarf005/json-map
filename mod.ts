fe929803e0b8c5669a8c5f7876b20ee8c80431c1
scarf <greenscarf005@gmail.com>
2025-04-20 21:58:33 +0900

    docs: add links

--- mod.ts
+++ mod.ts
@@ -1,7 +1,7 @@
 /**
  * JSON `parse` and `stringify` that uses [`Map`](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map) instead of `Object`. Can be used in a niche situation when you need to preserve the order of keys in JSON objects for some reason.
  *
- * Implemented as a thin wrapper around serde-json.
+ * Implemented as a thin wrapper around [serde_json](https://crates.io/crates/serde_json/1.0.1/dependencies).
  *
  * ## Example
  *
